e68e0235726b516d610263b303e3b9d8fafcaecf
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2024-11-21 15:33:59 +0500

    Update Model_Representation.py

--- Supervised Machine Learning/Tutorial Codes/Model_Representation.py
+++ Supervised Machine Learning/Tutorial Codes/Model_Representation.py
@@ -1,1 +1,101 @@
-print("Hello World")
+#Importing Numpy and Matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+
+#Creating training variables
+# x_train is the input variable (size in 1000 square feet)
+# y_train is the target (price in 1000s of dollars)
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+print(f"x_train = {x_train}")
+print(f"y_train = {y_train}")
+
+#Defining the shape or size of the array
+# m is the number of training examples
+print(f"x_train.shape: {x_train.shape}")
+m = x_train.shape[0]
+print(f"Number of training examples is: {m}")
+
+# We can also achieve the same using len function
+# m is the number of training examples
+m = len(x_train)
+print(f"Number of training examples is: {m}")
+
+# Calling the ith coordinate
+i = 0 # Change this to 1 to see (x^1, y^1)
+x_i = x_train[i]
+y_i = y_train[i]
+print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
+
+# Plotting the data using a scatter plot from a matplotlib library
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='k')
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.show()
+
+## Model function
+
+# <img align="left" src="./images/C1_W1_L3_S1_model.png"     style=" width:380px; padding: 10px; " > As described in lecture, the model function for linear regression (which is a function that maps from `x` to `y`) is represented as 
+
+# $$ f_{w,b}(x^{(i)}) = wx^{(i)} + b \tag{1}$$
+
+# The formula above is how you can represent straight lines - different values of $w$ and $b$ give you different straight lines on the plot. <br/> <br/> <br/> <br/> <br/> 
+
+# Let's try to get a better intuition for this through the code blocks below. Let's start with $w = 100$ and $b = 100$. 
+
+# **Note: You can come back to this cell to adjust the model's w and b parameters**
+
+# Defining w and b parameters for the model
+w = 100
+b = 100
+print(f"w: {w}")
+print(f"b: {b}")
+
+# Creating an array f, to hold values for the predictions of our model
+def compute_model_output(x, w, b):
+    """
+    Computes the prediction of a linear model
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      w,b (scalar)    : model parameters  
+    Returns
+      f_wb (ndarray (m,)): model prediction
+    """
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+        
+    return f_wb
+
+# Calling the function and computing the predictions
+tmp_f_wb = compute_model_output(x_train, w, b,)
+
+# Plot our model prediction
+plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
+
+# Making predictions through an modified version of our model
+w = 200                         
+b = 100    
+x_i = 1.2
+cost_1200sqft = w * x_i + b    
+
+print(f"${cost_1200sqft:.0f} thousand dollars")
