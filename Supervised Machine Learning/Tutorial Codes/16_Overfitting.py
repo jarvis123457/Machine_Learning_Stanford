2a31a60811f63c928195db8c982995fffb5fb819
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2024-12-09 18:52:05 +0500

    Create 16_Overfitting.py

---
+++ Supervised Machine Learning/Tutorial Codes/16_Overfitting.py
@@ -0,0 +1,11 @@
+%matplotlib widget
+import matplotlib.pyplot as plt
+from ipywidgets import Output
+from plt_overfit import overfit_example, output
+plt.style.use('./deeplearning.mplstyle')
+
+# Overfitting
+
+plt.close("all")
+display(output)
+ofit = overfit_example(False)
